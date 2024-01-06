b3605ed15a139318c32a94901303a9dd19846f5e
Tushar12152 <tusharimran470@gmail.com>
2024-01-06 10:29:39 +0600

    delete mobile from mobile collection

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tgzt8q2.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -69,6 +69,13 @@ async function run() {
          res.send(result)
     })
 
+    app.delete('mobiles/:id',async(req,res)=>{
+          const id=req.params.id;
+          const query={_id:new ObjectId(id)}
+          const result=await mobileCollection.deleteOne(query)
+          res.send(result)
+    })
+
 
 
 
@@ -76,7 +83,7 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("Pinged your deployment. You successfully connected to Mong oDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
